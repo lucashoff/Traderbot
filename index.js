324169e1c0b0be3797126d906966bd076a169428
lucashoff <lucaspnsh@gmail.com>
2021-04-12 00:46:21 -0300

    função para calcular valor limite de compra

--- index.js
+++ index.js
@@ -14,46 +14,58 @@ var tradeApi = new MercadoBitcoinTrade({
 
 // Functions
 function getQuantity(coin, price, isBuy, callback){
-    //pega preço e atribui ao "coin" Reais caso for operação de compra
-    price = parseFloat(price);
-    coin = isBuy ? 'brl' : coin.toLowerCase();
- 
-    tradeApi.getAccountInfo((response_data) => {
-        // busca saldo da criptomoeda ou em reais na conta
-        var balance = parseFloat(response_data.balance[coin].available).toFixed(5);
-        balance = parseFloat(balance);
-        //verifica se tem no mínimo R$ 10,00 de saldo para compra
-        if(isBuy && balance < 10) return console.log('Saldo insuficiente para comprar!');
-        console.log(`Saldo disponível de ${coin}: ${balance}`);
-        // caso for compra, divide-se o saldo em reais pelo preço da moeda para definir a quantidade de compra
-        if(isBuy) balance = parseFloat((balance / price).toFixed(5));
-        callback(parseFloat(balance) - 0.00001);//tira a diferença que se ganha no arredondamento
-    }, 
-    (data) => console.log(data))
+   //pega preço e atribui ao "coin" Reais caso for operação de compra
+   price = parseFloat(price);
+   coin = isBuy ? 'brl' : coin.toLowerCase();
+
+   tradeApi.getAccountInfo((response_data) => {
+      // busca saldo da criptomoeda ou em reais na conta
+      var balance = parseFloat(response_data.balance[coin].available).toFixed(5);
+      balance = parseFloat(balance);
+      //verifica se tem no mínimo R$ 10,00 de saldo para compra
+      if(isBuy && balance < 10) return console.log('Saldo insuficiente para comprar!');
+      console.log(`Saldo disponível de ${coin}: ${balance}`);
+      // caso for compra, divide-se o saldo em reais pelo preço da moeda para definir a quantidade de compra
+      if(isBuy) balance = parseFloat((balance / price).toFixed(5));
+      callback(parseFloat(balance) - 0.00001);//tira a diferença que se ganha no arredondamento
+   }, 
+   (data) => console.log(data))
+}
+
+function getMaxValueToBuy(low, high){
+   low = parseFloat(low);
+   high = parseFloat(high);
+   var diff = (high - low);
+   var maxValue = low + parseFloat(diff * 0.4); // calcula valor limite de compra com 40% do spread
+   return parseFloat(maxValue).toFixed(2);
 }
 
 setInterval(() => 
-    infoApi.ticker((response) => {
-        console.log(response.ticker);
-        // verifica o valor de venda da criptomoeda
-        if(response.ticker.sell <= 90000){
-            // busca saldo em reais para compra
-            getQuantity('BRL', response.ticker.sell, true, (qty) => {
-                //cria ordem de compra
-                tradeApi.placeBuyOrder(qty, response.ticker.sell, 
-                    (data) => {
-                        console.table('Ordem de compra inserida no livro. ' + data.order)
+   infoApi.ticker((response) => {
+      console.log(response.ticker);
+      // verifica o valor de venda da criptomoeda
+      var limitValueToBuy = getMaxValueToBuy(response.ticker.low, response.ticker.high);
+      console.log('Valor limite de compra:'+limitValueToBuy);
+      if(response.ticker.sell <= limitValueToBuy){
+         // busca saldo em reais para compra
+         getQuantity('BRL', response.ticker.sell, true, (qty) => {
+               //cria ordem de compra
+               tradeApi.placeBuyOrder(qty, response.ticker.sell, 
+                  (data) => {
+                     console.table('Ordem de compra inserida no livro. ' + data.order)
 
-                        setTimeout(() =>
-                            //operando em STOP
-                            tradeApi.placeSellOrder((parseFloat(data.order.quantity - data.order.fee).toFixed(5) - 0.00001), parseFloat(response.ticker.sell * parseFloat(Profit)).toFixed(5), 
-                                (data) => console.log('Ordem de venda inserida no livro. ' + data),
-                                (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data)),
-                                2000)
-                    },
-                    (data) => console.table('Erro ao inserir ordem de compra no livro. ' + data))
-            })
-        }
-    }),
+                     setTimeout(() =>
+                           //operando em STOP
+                           tradeApi.placeSellOrder((parseFloat(data.order.quantity - data.order.fee).toFixed(5) - 0.00001), parseFloat(response.ticker.sell * parseFloat(Profit)).toFixed(5), 
+                              (data) => console.log('Ordem de venda inserida no livro. ' + data),
+                              (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data)),
+                              2000)
+                  },
+                  (data) => console.table('Erro ao inserir ordem de compra no livro. ' + data))
+         });
+      }else{
+         console.log("Valor atual da moeda maior que o limite estipulado.");
+      }
+   }),
    process.env.CRAWLER_INTERVAL
 )
--- index_old.js
+++ index_old.js
@@ -3,6 +3,7 @@ const MercadoBitcoinDados = require("./api").MercadoBitcoinDados
 const MercadoBitcoinTrade = require("./api").MercadoBitcoinTrade
 const Coin = process.env.COIN;
 const Profit = process.env.PROFIT;
+const Interval = process.env.CRAWLER_INTERVAL;
 var infoApi = new MercadoBitcoinDados({ currency: process.env.COIN});
 var tradeApi = new MercadoBitcoinTrade({
    currency: process.env.COIN,
@@ -11,112 +12,50 @@ var tradeApi = new MercadoBitcoinTrade({
    pin: process.env.PIN
 });
 
-// code Lucas: pegar saldo
-function getBalance(coin, callback){
-   coin = coin ? coin : 'brl';
-   coin = coin.toLowerCase();
-   tradeApi.getAccountInfo((response_data) => {
-      var balance = parseFloat(response_data.balance[coin].available).toFixed(5);   
-      balance = parseFloat(balance);
-      // if (coin != 'brl'){
-      //    console.log("o saldo é: "+coin.toUpperCase()+" "+ balance);
-      // }else{
-      //    console.log("o saldo em reais é: R$ " + balance);
-      // }
-      return callback(balance);
-   });
+// Functions
+function getQuantity(coin, price, isBuy, callback){
+    //pega preço e atribui ao "coin" Reais caso for operação de compra
+    price = parseFloat(price);
+    coin = isBuy ? 'brl' : coin.toLowerCase();
+ 
+    tradeApi.getAccountInfo((response_data) => {
+        // busca saldo da criptomoeda ou em reais na conta
+        var balance = parseFloat(response_data.balance[coin].available).toFixed(5);
+        balance = parseFloat(balance);
+        //verifica se tem no mínimo R$ 10,00 de saldo para compra
+        if(isBuy && balance < 10) return console.log('Saldo insuficiente para comprar!');
+        console.log(`Saldo disponível de ${coin}: ${balance}`);
+        // caso for compra, divide-se o saldo em reais pelo preço da moeda para definir a quantidade de compra
+        if(isBuy) balance = parseFloat((balance / price).toFixed(5));
+        callback(parseFloat(balance) - 0.00001);//tira a diferença que se ganha no arredondamento
+    }, 
+    (data) => console.log(data))
 }
 
-function getCoinValue(){
-   infoApi.ticker((response) => {
-      var lastValue = parseFloat(response.ticker.last).toFixed(5);
-      console.log("o valor atual unitário de "+Coin+" é: R$ "+lastValue);
-   });
-}
+setInterval(() => 
+    infoApi.ticker((response) => {
+        console.log(response.ticker);
+        // verifica o valor de venda da criptomoeda
+        if(response.ticker.sell <= 305000){
+            // busca saldo em reais para compra
+            getQuantity('BRL', response.ticker.sell, true, (qty) => {
+                //cria ordem de compra
+                tradeApi.placeBuyOrder(qty, response.ticker.sell, 
+                    (data) => {
+                        console.table('Ordem de compra inserida no livro. ' + data.order)
 
-function buyOrder(coin, value){
-   tradeApi.placeBuyOrder(qty, response.ticker.sell, (data) => {
-      console.log('Ordem de compra inserida no livro. '+data);
-      //operando em STOP
-      tradeApi.placeSellOrder(data.quantity, response.ticker.sell * parseFloat());
-   });
-}
-
-// execução do robô
-setInterval(() => {
-   var balance;
-   // getBalance(Coin, (response) =>{
-   //    getCoinValue(); //2°
-   //    console.log('o saldo de '+Coin+' é: '+response); //1°
-   // });
-   getQuantity(Coin, response.ticker.sell, true, (qty) =>{
-      tradeApi.placeBuyOrder(qty, response.ticker.sell, 
-         (data) => {
-            console.log('Ordem de compra inserida no livro. '+data);
-            //operando em STOP
-            tradeApi.placeSellOrder(data.quantity, response.ticker.sell * parseFloat())
-         });
-   });
-   console.log('------------------------------');
-   },
+                        setTimeout(() =>
+                            //operando em STOP
+                            tradeApi.placeSellOrder((parseFloat(data.order.quantity - data.order.fee).toFixed(5) - 0.00001), parseFloat(response.ticker.sell * parseFloat(Profit)).toFixed(5), 
+                                (data) => console.log('Ordem de venda inserida no livro. ' + data),
+                                (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data)),
+                                2000)
+                    },
+                    (data) => console.table('Erro ao inserir ordem de compra no livro. ' + data))
+            })
+        }else{
+            console.log("Valor atual da moeda maior que o limite estipulado.");
+        }
+    }),
    process.env.CRAWLER_INTERVAL
 )
-
-
-
-// var saldo = getBalance(process.env.COIN);
-// console.log('o saldo em '+process.env.COIN+' é: '+saldo);
-// setInterval(() => 
-//    getBalance('BCH');
-//    process.env.CRAWLER_INTERVAL
-// )
-
-// code da net
-function getQuantity(coin, price, isBuy, callback){
-   price = parseFloat(price);
-   coin = isBuy ? 'brl' : coin.toLowerCase();
-
-   tradeApi.getAccountInfo((response_data) => {
-      var saldo = parseFloat(response_data.balance[coin].available).toFixed(5);   
-      saldo = parseFloat(saldo);
-
-      console.log('saldo em '+coin+': '+saldo);
-      // if(isBuy && balance < 50){
-      //    return console.log('Sem saldo disponível para comprar!'); 
-      // }else{
-      //    console.log(`Saldo disponível de ${coin}: ${balance}`);
-      // }
-
-      // if(isBuy) balance = parseFloat((balance / price).toFixed(5))
-      //    callback(parseFloat(balance) - 0.00001)//tira a diferença que se ganha no arredondamento
-   },
-   (data) => console.log(data));
-}
-
-// setInterval(() => 
-//    infoApi.ticker((tick) => console.log(tick.ticker)),
-//    process.env.CRAWLER_INTERVAL
-// )
-
-// setInterval(() => 
-//    infoApi.ticker((response) => {
-//       console.log(response.ticker);
-//       if(response.ticker.last >= 1340){
-//          getQuantity(process.env.COIN, response.ticker.sell, true, (qty) =>{
-//             tradeApi.placeBuyOrder(qty, response.ticker.sell, 
-//                (data) => {
-//                   console.log('Ordem de compra inserida no livro. '+data);
-//                   //operando em STOP
-//                   tradeApi.placeSellOrder(data.quantity, response.ticker.sell * parseFloat())
-//                });
-//          });
-//          // tradeApi.placeSellOrder(0.001, 1.35,
-//          //    (data) => console.log('Ordem de venda inserida no livro. ' + data),
-//          //    (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data))
-//       }
-//       else{
-//          console.log('Abaixo do valor esperado.');
-//       }
-//    }),
-//    process.env.CRAWLER_INTERVAL
-// );
